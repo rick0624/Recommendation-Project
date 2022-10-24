8ee7be3a85775923669157aac7955582e55e3cd3
Rick Wu (RD-TW-INTRN) <rick_wu@trendmicro.com>
2022-10-24 21:02:59 +0800

    fix google redirect to main page

--- routes/auth-route.js
+++ routes/auth-route.js
@@ -61,9 +61,9 @@ router.get("/google/redirect", passport.authenticate("google"), (req, res) => {
     if (req.session.returnTo){
         let newPath = req.session.returnTo;
         req.session.returnTo = ""
-        res.redirect(newPath);
+        res.redirect("/");
     }else{
-        res.redirect("/profile");
+        res.redirect("/");
     }
 });
 
