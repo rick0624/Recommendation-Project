7d2d9efd43cf3ccbda16e944fd9912ad8c856d05
Rick Wu (RD-TW-INTRN) <rick_wu@trendmicro.com>
2022-11-12 23:53:47 +0800

    improve image src

--- routes/classify-route.js
+++ routes/classify-route.js
@@ -8,23 +8,28 @@ router.get("/type/:type", async (req, res) => {
     console.log("-----------------------------------");
     const product_type = req.params.type;
     let good_detailFound = await Good_Detail.find({classify : product_type});
-    let recommedationFound = await Recommendation.find({ buyer: req.user._id});
-    let recommendation_array = [];
-    for(let i=0; i<recommedationFound.length; i++){
-        let good_Found = await Good_Detail.find({good_id : recommedationFound[i].good});
-        recommendation_array.push(good_Found[0]);
+    let recommendation_array = []
+    if(req.user){
+        let recommedationFound = await Recommendation.find({ buyer: req.user._id});
+        for(let i=0; i<recommedationFound.length; i++){
+            let good_Found = await Good_Detail.find({good_id : recommedationFound[i].good});
+            recommendation_array.push(good_Found[0]);
+        }
     }
+    
     res.render("classify", {user: req.user, good_details : good_detailFound, type:product_type, recommendation_goods : recommendation_array});
 });
 
 router.get("/color/:color", async (req, res) => {
     const product_color = req.params.color;
     let good_detailFound = await Good_Detail.find({color : product_color});
-    let recommedationFound = await Recommendation.find({ buyer: req.user._id});
-    let recommendation_array = [];
-    for(let i=0; i<recommedationFound.length; i++){
-        let good_Found = await Good_Detail.find({good_id : recommedationFound[i].good});
-        recommendation_array.push(good_Found[0]);
+    let recommendation_array = []
+    if(req.user){
+        let recommedationFound = await Recommendation.find({ buyer: req.user._id});
+        for(let i=0; i<recommedationFound.length; i++){
+            let good_Found = await Good_Detail.find({good_id : recommedationFound[i].good});
+            recommendation_array.push(good_Found[0]);
+        }
     }
     res.render("color", {user: req.user, good_details : good_detailFound, color:product_color, recommendation_goods : recommendation_array});
 })
